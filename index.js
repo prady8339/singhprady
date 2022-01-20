39d6bba8b5f2842ff3f3ddd6972b4f086e2dc7ba
prady8339 <76277136+prady8339@users.noreply.github.com>
2022-01-20 10:28:09 +0530

    js update

---
+++ index.js
@@ -0,0 +1,14 @@
+// var React = require("react");
+// var ReactDOM = require("react-dom");
+// const year = new Date().getFullYear();
+
+// document.getElementById("year").innerHTML(`year`)
+
+
+// ReactDOM.render(
+//     <div>
+//       <p> </p>
+//       <p>copyright {year}</p>
+//     </div>,
+//     document.getElementById("year")
+//   );
