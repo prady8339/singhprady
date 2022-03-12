f46f3e05b0aee0bc828198183ffb9be8f2c7e3ca
prady8339 <76277136+prady8339@users.noreply.github.com>
2022-03-12 23:48:41 +0530

    github api projects

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-// var React = require("react");
-// var ReactDOM = require("react-dom");
-// const year = new Date().getFullYear();
+var request = new XMLHttpRequest();
 
-// document.getElementById("year").innerHTML(`year`)
+request.open('GET','https://api.github.com/users/prady8339/repos ',true);
+
+request.onload = function(){
+
+  var data = JSON.parse(this.response)
+  console.log(data);
+  var statusHTML = "";
 
 
-// ReactDOM.render(
-//     <div>
-//       <p> </p>
-//       <p>copyright {year}</p>
-//     </div>,
-//     document.getElementById("year")
-//   );
+$.each(data,function(i,status){
+
+  statusHTML += '<div class="card">';
+  statusHTML += '<img class="card-img-top" src="https://avatars.githubusercontent.com/u/76277136?v=4" alt="Card image cap">';
+  
+  statusHTML +=  '<div class="card-body"> <h5 class="card-title">';
+  statusHTML += status.name;
+  statusHTML += '</h5> <p class="card-text"> ';
+  statusHTML += status.description+'</p>';
+ 
+  // $.each(status.contributors_url,function(j,contributers){
+  //   statusHTML += '<p>'+contributers.login+'</p>';
+  // });
+
+  statusHTML += '<a href="'+status.html_url;
+  statusHTML += '" class="btn btn-primary">Github</a></div> ';
+  statusHTML += '</div>';
+});
+
+document.getElementById('halwa').innerHTML = statusHTML;
+
+}
+request.send();
+
